583c4ef36af35762fe5806c1c847e85b44a50a51
ndominkiewicz <norbertd1410@gmail.com>
2025-04-28 00:18:34 +0200

    full task

--- src/Integral.java
+++ src/Integral.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.function.Function;
 public class Integral {
     private double a;
@@ -18,8 +19,44 @@ public class Integral {
         }
         System.out.println("Trapezoid: " + result);
     }
+    public void simpsonMethod(int n){
+        double h;
+        double result = 0;
+        ArrayList<Double> xi = new ArrayList<>();
+        ArrayList<Double> ti = new ArrayList<>();
+        for(double i = 0; i <= n; i++) {
+            xi.add(a + (i / n) * (b - a));
+        }
+        h = (xi.getLast() - xi.get(xi.size()-2))/2;
+        for(double i = 0; i < n; i++) {
+            ti.add((xi.get((int)i+1) + xi.get((int)i))/2);
+        }
+        result += mathFunction.apply(xi.getFirst());
+        for(int i = 0; i < n; i++) {
+            result += 4 * mathFunction.apply(ti.get(i));
+        }
+        for(int i = 1; i < n; i++) {
+            result += 2 * mathFunction.apply(xi.get(i));
+        }
+        result += mathFunction.apply(xi.get(n));
+        result *= h/3;
+        System.out.println("Simpson: " + result);
+    }
+    public void numericalMethod(int n,boolean writeOutput){
+        double result = 0;
+        double multiplier = (b - a) / 2;
+        Scrapper scrapper = new Scrapper(n,writeOutput);
+        Element element = scrapper.getElement(n);
+        for(int i = 0; i < n; i++) {
+            result += element.w.get(i) * mathFunction.apply(multiplier * element.t.get(i) + ((b + a)/2));
+        }
+        result *= multiplier;
+        System.out.println("Numerical: " + result);
+    }
     public static void main(String[] args) {
         Integral integral = new Integral(0.5, 1.8,x -> (Math.cos(Math.pow(x, 3) + 0.7))/(1.1 + Math.cos(0.3 * x + 0.1)));
         integral.trapezoidMethod(4);
+        integral.simpsonMethod(4);
+        integral.numericalMethod(4,false);
     }
 }
